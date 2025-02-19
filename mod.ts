84d94565207e3957d3a150b331fb41c4b762c092
Wojciech Pawlik <woj.pawlik@gmail.com>
2025-02-19 16:34:26 +0100

    🚀 Publish to JSR

--- mod.ts
+++ mod.ts
@@ -1,1 +1,6 @@
+#!/usr/bin/env node
 export * from "./src/mod.ts";
+import { argv } from "node:process";
+if (import.meta.main ?? import.meta.filename === argv[1]) {
+  await import("./src/cli" + argv[1].slice(-3));
+}
