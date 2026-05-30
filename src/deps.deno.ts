d6ec0e9960e5da99a304672a9de88d26494ac214
Wojciech Pawlik <woj.pawlik@gmail.com>
2026-05-30 10:06:53 +0200

    Upgrade to TypeScript 6.0

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -1,3 +1,3 @@
 // Deno-only, see https://doc.deno.land/https/deno.land/x/deno2node/src/mod.ts#deno2node
 // Auto-updated in `dependencies` script
-export * from "jsr:@ts-morph/ts-morph@27.0.0";
+export * from "jsr:@ts-morph/ts-morph@28.0.0";
